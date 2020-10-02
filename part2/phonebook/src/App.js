09953db691aa53e2b6fbc95f6d2c6a53db6722ee
cdrbbt <egor.bykadorov@gmail.com>
2020-10-02 20:02:39 +0300

    2.9 phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
 
-const defaultPerson = { name: '', pNumber: '' }
+const defaultPerson = { name: '', number: '' }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', pNumber: '041-32165478' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newPerson, setNewPerson] = useState(defaultPerson)
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -18,18 +22,24 @@ const App = () => {
     }
   }
 
-  const nameChange = (event) => setNewPerson({...newPerson, name:event.target.value})
-  const numberChange = (event) => setNewPerson({...newPerson, pNumber:event.target.value})
+  const nameChange = (event) => setNewPerson({ ...newPerson, name: event.target.value })
+  const numberChange = (event) => setNewPerson({ ...newPerson, number: event.target.value })
+  const filterChange = (event) => setFilter(event.target.value)
+  const fileteredList = persons.filter((person) => person.name.toLowerCase().includes(filter.toLocaleLowerCase()))
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter: <input value={filter} onChange={filterChange}/>
+      </div>
+      <h2>Add a new person</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newPerson.name} onChange={nameChange} />
         </div>
         <div>
-          number: <input value={newPerson.pNumber} onChange={numberChange}/>
+          number: <input value={newPerson.number} onChange={numberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -37,7 +47,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name} {person.pNumber}</li>)}
+        {fileteredList.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
     </div>
 
