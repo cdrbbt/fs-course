bc8b9300d1d2fdaaab2d4feb140f531e50de35df
cdrbbt <egor.bykadorov@gmail.com>
2020-10-05 17:14:55 +0300

    2.14 data for countries

--- part2/dataforcountries/src/components/CountryInfo.js
+++ part2/dataforcountries/src/components/CountryInfo.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const CountryInfo = ({ country }) => {
+
+  const [weather, setWeather] = useState(null)
+
+
+  useEffect(() => {
+    const params = {
+      access_key: process.env.REACT_APP_WEATHER_KEY,
+      query: country.capital
+    }
+    axios
+      .get('http://api.weatherstack.com/current', { params })
+      .then(response => {
+        console.log(response)
+        setWeather(response.data)
+      })
+  }, [country.capital])
+
   return (
     <div>
       <h1>{country.name}</h1>
-      <p>Capital: {country.capital}</p>
       <p>Population: {country.population}</p>
       <h2>Languages:</h2>
       <ul>
@@ -12,6 +29,10 @@ const CountryInfo = ({ country }) => {
       </ul>
       <h2>Flag:</h2>
       <img src={country.flag} alt='flag' height='50' />
+      <p>Capital: {country.capital}</p>
+      {weather ? <p>Weather: {weather.current.temperature}</p> : null}
+      {weather ? <img src={weather.current.weather_icons[0]} alt='weather icon' /> : null}
+      {weather ? <p>Wind {weather.current.wind_speed} mph in {weather.current.wind_dir} direction</p> : null}
     </div>
   )
 }
